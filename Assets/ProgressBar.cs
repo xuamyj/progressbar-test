6fb664e8a67fd8399f7e3461c453f071df6b9241
Amy Xu <xuamyj@gmail.com>
2024-12-10 17:38:06 -0500

    progress bar part 1

---
+++ Assets/ProgressBar.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public int maximum;
+    public int current;
+    public Image mask;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+    }
+}
